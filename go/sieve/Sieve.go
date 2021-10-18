74450cb04acabcdadbf0d50350c431b27892c535
Kris Foster <me@krismade.me>
2021-10-18 17:59:11 +0000

    Added go implementatiosn plus test for prime sieve

---
+++ go/sieve/Sieve.go
@@ -0,0 +1,73 @@
+package main
+
+/**
+ * This implemenation is taken from:
+ * https://www.socketloop.com/tutorials/golang-sieve-of-eratosthenes-algorithm
+ */
+
+import (
+	"flag"
+	"fmt"
+)
+
+var (
+	displayFlag *bool
+	upper       *int
+)
+
+func displayPrime(primes []int, displayFlag bool) {
+	for i := 0; i < len(primes); i++ {
+		if displayFlag {
+			fmt.Printf("%d ", primes[i])
+		}
+	}
+	if displayFlag {
+		fmt.Println()
+	}
+	fmt.Printf("Last Prime: %d\n", primes[len(primes)-1])
+}
+
+func sieve(num int) []int {
+
+	// do not use
+	// prime := [num+1]bool
+	// will cause : non-constant array bound num + 1 error
+
+	// an array of boolean - the idiomatic way
+	primes := make([]bool, num+1)
+	foundPrimes := make([]int, num)
+
+	// initialize everything with false first(not crossed)
+	for i := 0; i < num+1; i++ {
+		primes[i] = true
+	}
+
+	for i := 2; i*i <= num; i++ {
+		if primes[i] == true {
+			for j := i * 2; j <= num; j += i {
+				primes[j] = false // cross
+			}
+		}
+	}
+
+	cnt := 0
+	for i := 2; i < len(primes); i++ {
+		if primes[i] == true {
+			foundPrimes[cnt] = i
+			cnt++
+		}
+	}
+
+	return foundPrimes[0:cnt]
+}
+
+func init() {
+	displayFlag = flag.Bool("display", false, "Used to turn on displaying found primes")
+	upper = flag.Int("upper", 1000000, "Upper limit to look for primes below")
+}
+
+func main() {
+	flag.Parse()
+	primes := sieve(*upper)
+	displayPrime(primes, *displayFlag)
+}
---
+++ go/sieve/Sieve_test.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestSieveFirstUnderHundredPrimes(t *testing.T) {
+	primes := [25]int{2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43,
+		47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97}
+	myPrimes := sieve(100)
+	if len(primes) != len(myPrimes) {
+		t.Fatalf(`sieve(100) = only returns %d`, len(primes))
+	}
+	for i := 0; i < len(primes); i++ {
+		if primes[i] != myPrimes[i] {
+			t.Fatalf(`prime[%d] != myprimes[%d]`, i, i)
+		}
+	}
+}
